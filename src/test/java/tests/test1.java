d6cbcbe4e0513efd16526a2378309b0b07d0ab92
amira fathy <amirafathy947@gmail.com>
2023-08-04 17:22:13 +0300

    Initial commit

---
+++ src/test/java/tests/test1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import com.aventstack.extentreports.model.Report;
+import com.shaft.tools.io.ReportManager;
+import org.testng.annotations.Test;
+
+
+
+public class test1 {
+    @Test
+            public void hay() {
+        ReportManager.log("HAY");
+    }
+}
